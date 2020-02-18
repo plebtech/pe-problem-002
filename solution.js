43f2e32c37b21256f869b3c6909eab778298e5f5
Nick Mc <nbmc@protonmail.com>
2020-02-18 12:39:07 -0600

    solved.

---
+++ solution.js
@@ -0,0 +1,15 @@
+// sum of even fibonacci numbers below four million.
+let num1 = 0;
+let num2 = 1;
+let current = 0;
+let sum = 0;
+//first 100 fibonacci numbers.
+while (current < 4000000) {
+    current = num1 + num2;
+    num1 = num2;
+    num2 = current;
+    if (current % 2 === 0) {
+        sum += current;
+    }
+}
+console.log(sum);
